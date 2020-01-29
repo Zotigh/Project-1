8ae9e62ef3a0bafdade61cd3293f436388a7c296
lzotigh1 <lzotigh1@CISP15>
2020-01-29 11:46:28 -0700

    Add project files.

---
+++ ZotighP1/ZotighP1.cpp
@@ -0,0 +1,54 @@
+// ZotighP1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Programmer: Lance Zotigh (lzotigh1@cnm.edu)
+// Date: 1/27/2020
+// Pourpose: Calculate profit for moive theator
+
+#include <iostream>
+
+int main()
+{
+   //Declare constants
+    const float PERCENTKEPTBYTHEATER = 0.2;
+    const float ADULTTICKETPRICE = 10.50;
+    const float CHILDTICKETPRICE = 6.25;
+    string movieName;
+
+   //Declare variables 
+
+   //Output header
+      
+   //ask user for the name of movie
+
+   //get # of adult tickets
+      
+   //get # of children
+
+   //Cal adult rev
+
+   //Cal child rev
+
+   //cal gross profit
+
+   //cal net profit
+
+   //cal money sent to distributer
+
+   //report results showing # of adults
+
+   //Most of these are cout statements
+   // tickets, the number of child tickets
+   // the revenue from each category of ticket
+   //gross the profit and
+   //the amount sent to the ditributer
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
